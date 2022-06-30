4d7cf65c569314a5dcb706095268f28f8e3b8513
kluberge <kibiklu@gmail.com>
2022-06-30 22:37:45 +1000

    finally put this bs on github

---
+++ BetterBensRaincoat/BetterBensRaincoat.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Collections;
+using BepInEx;
+using BepInEx.Configuration;
+using IL.RoR2.Items;
+using R2API;
+using R2API.Utils;
+using RoR2;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace BetterBensRaincoat
+{
+    //This is an example plugin that can be put in BepInEx/plugins/BetterBensRaincoat/BetterBensRaincoat.dll to test out.
+    //It's a small plugin that adds a relatively simple item to the game, and gives you that item whenever you press F2.
+
+    //This attribute specifies that we have a dependency on R2API, as we're using it to add our item to the game.
+    //You don't need this if you're not using R2API in your plugin, it's just to tell BepInEx to initialize R2API before this plugin so it's safe to use R2API.
+    [BepInDependency(R2API.R2API.PluginGUID)]
+
+    //This attribute is required, and lists metadata for your plugin.
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+
+    //We will be using 2 modules from R2API: ItemAPI to add our item and LanguageAPI to add our language tokens.
+    [R2APISubmoduleDependency(nameof(ItemAPI), nameof(LanguageAPI), nameof(RecalculateStatsAPI))]
+
+    //This is the main declaration of our plugin class. BepInEx searches for all classes inheriting from BaseUnityPlugin to initialize on startup.
+    //BaseUnityPlugin itself inherits from MonoBehaviour, so you can use this as a reference for what you can declare and use in your plugin class: https://docs.unity3d.com/ScriptReference/MonoBehaviour.html
+    public class BetterBensRaincoat : BaseUnityPlugin
+    {
+        //The Plugin GUID should be a unique ID for this plugin, which is human readable (as it is used in places like the config).
+        //If we see this PluginGUID as it is on thunderstore, we will deprecate this mod. Change the PluginAuthor and the PluginName !
+        public const string PluginGUID = PluginAuthor + "." + PluginName;
+        public const string PluginAuthor = "PhysicsFox";
+        public const string PluginName = "BetterBensRaincoat";
+        public const string PluginVersion = "1.1.0";
+
+        private static ConfigFile config;
+        private static ConfigEntry<float> configBarrierValue;
+        private static ConfigEntry<bool> configVfx;
+        
+        //The Awake() method is run at the very start when the game is initialized.
+        public void Awake()
+        {
+            //Init our logging class so that we can properly log for debugging
+            Log.Init(Logger);
+            config = base.Config;
+
+            On.RoR2.Items.ImmuneToDebuffBehavior.OverrideDebuff_BuffDef_CharacterBody += (orig, buffDef, body) =>
+            {
+                return buffDef.buffIndex != BuffIndex.None && buffDef.isDebuff && TryApplyOverride(body);
+            };
+            
+            On.RoR2.Items.ImmuneToDebuffBehavior.OverrideDot += (orig, inflictDotInfo) =>
+            {
+                GameObject victimObject = inflictDotInfo.victimObject;
+                CharacterBody characterBody = (victimObject != null) ? victimObject.GetComponent<RoR2.CharacterBody>() : null;
+                return characterBody && TryApplyOverride(characterBody);
+            };
+
+            // Update description to match functionality
+            var pickupToken = "ITEM_IMMUNETODEBUFF_PICKUP";
+            var descToken = "ITEM_IMMUNETODEBUFF_DESC";
+            LanguageAPI.Add(pickupToken,
+                "Prevent debuffs, instead gaining a temporary barrier.");
+            LanguageAPI.Add(descToken,
+                "Whenever you would receive a debuff, instead receive <style=cIsUtility>10%</style> barrier <style=cStack>(+10% barrier per stack)</style>.");
+
+            var section = "ItemAttributes";
+            configBarrierValue = config.Bind(section, "BarrierAmount", 0.1f, 
+                "Amount of barrier each stack of Ben's Raincoat gives per debuff");
+            configVfx = config.Bind(section, "ShowVfx", true, 
+                "Whether or not the raincoat effect should play. May want to hide VFX if photosensitive or just find it annoying");
+
+            Log.LogInfo(nameof(Awake) + " done.");
+        }
+
+        private static bool TryApplyOverride(CharacterBody body)
+        {
+            RoR2.Items.ImmuneToDebuffBehavior component = body.GetComponent<RoR2.Items.ImmuneToDebuffBehavior>();
+            if (component)
+            {
+                if (component.healthComponent)
+                {
+                    // Stacks 10% barrier per debuff taken per stack
+                    component.healthComponent.AddBarrier(configBarrierValue.Value * component.stack * component.healthComponent.fullCombinedHealth);
+                    
+                    // Play VFX?
+                    if (configVfx.Value)
+                        EffectManager.SimpleImpactEffect(Addressables.LoadAssetAsync<GameObject>("RoR2/DLC1/ImmuneToDebuff/ImmuneToDebuffEffect.prefab").WaitForCompletion(), body.corePosition, Vector3.up, true);
+                    
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void RefreshBuffCount(CharacterBody body)
+        {
+            var coatCount = body.inventory.GetItemCount(RoR2.Items.ImmuneToDebuffBehavior.GetItemDef());
+            body.SetBuffCount(DLC1Content.Buffs.ImmuneToDebuffReady.buffIndex, coatCount);
+        }
+        
+        public void FixCounts()
+        {
+            // Fix Ben's Raincoat stack not updating in the buff bar
+            var instances = PlayerCharacterMasterController.instances;
+            if (instances == null)
+                return;
+        
+            foreach (PlayerCharacterMasterController playerCharacterMaster in instances)
+            {
+                var body = playerCharacterMaster.master.GetBody();
+                if (body == null)
+                    continue;
+                RefreshBuffCount(body);
+            }
+        }
+
+        public void FixedUpdate()
+        {
+            FixCounts();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,44 @@
+# Better Ben's Raincoat
+
+Ben's Raincoat: it's not a good item. 
+
+You're like me; when SOTV first came out, you were shocked and excited by Ben's Raincoat. Negate all debuffs? Sounds busted, but also fun to get in a vanilla run! 
+
+Fun, that's the game design pillar that Hopoo Games has increasingly neglected. 
+
+This is not just a mod, but a message from the community that at the end of the day, fun is more important than balance. Risk of Rain 2 is a game, not an interactive spreadsheet (like **some people** would claim).
+
+To this end, I've created this mod to tweak Ben's Raincoat as follows:
+
+![](https://cdn.discordapp.com/attachments/979879658160021556/980328005916299354/unknown.png)
+
+**Ben's Raincoat**
+
+Every debuff received is converted into *10%* barrier *(+10% per stack)*, and this amount is configurable. 
+
+This essentially just takes the pre-patch raincoat and buffs it so that it scales with number of stacks.
+
+## Known Issues
+
+* None
+
+To report an issue, feel free to ping me on Discord @physicsfox#7810 
+
+## Changelog
+
+**1.1.0**
+* Buffed configurability, including photosensitivity option that disables VFX
+* Buff README
+
+**1.0.2**
+* Fixed item description and pickup tag
+* Fixed buff display 
+* Nerfed typos 
+
+**1.0.1**
+
+* Change 5% barrier to 10% barrier
+
+**1.0.0**
+
+* Release of my first mod.
